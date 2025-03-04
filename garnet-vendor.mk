57bc922b4aae30861eb57054c852d4910cc68a26
kleidione Freitas <kleidione@gmail.com>
2025-03-03 22:21:37 -0300

    Revert "garnet: Rename audio primary HAL"
    This reverts commit e723a13.
    
    Signed-off-by: Kleidione <kleidione@gmail.com>

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -1152,7 +1152,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss-aidl-impl-qti \
     android.hardware.gnss@2.1-impl-qti \
     audio.bluetooth_qti.default \
-    audio.primary.garnet \
+    audio.primary.parrot \
     camera.qcom \
     camera.xiaomi \
     com.dsi.ant@1.0-impl \
